ae4ed7b168f4d13253077a669583396c449258c8
David H. Bronke <whitelynx@gmail.com>
2020-11-22 16:32:04 -0500

    fix(project.clj): corrected :license to match the actual license of the project

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject dactyl-keyboard "0.1.0-SNAPSHOT"
   :description "A parametrized, split-hand, concave, columnar, erogonomic keyboard"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "GNU Affero General Public License version 3"
+            :url "https://www.gnu.org/licenses/agpl-3.0.en.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [unicode-math "0.2.0"]
                  [scad-clj "0.5.3"]])
