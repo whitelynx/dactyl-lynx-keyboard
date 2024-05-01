e5035a87b53e15e7342ccf7a89b0b53cfb206262
David H. Bronke <whitelynx@gmail.com>
2024-05-01 13:40:19 +0200

    Removed original Dactyl source and docs, since they aren't relevant any more

--- project.clj
+++
@@ -1,9 +0,0 @@
-(defproject dactyl-keyboard "0.1.0-SNAPSHOT"
-  :description "A parametrized, split-hand, concave, columnar, erogonomic keyboard"
-  :url "http://example.com/FIXME"
-  :license {:name "GNU Affero General Public License version 3"
-            :url "https://www.gnu.org/licenses/agpl-3.0.en.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/math.numeric-tower "0.0.4"]
-                 [unicode-math "0.2.0"]
-                 [scad-clj "0.5.3"]])
