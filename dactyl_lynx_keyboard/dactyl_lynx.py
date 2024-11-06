c85ac5660ad1be8b49b5a6619311b2106b636e56
David H. Bronke <whitelynx@gmail.com>
2024-11-06 12:52:14 +0100

    fix(dactyl_lynx): added TrackPointMount to right side

--- dactyl_lynx_keyboard/dactyl_lynx.py
+++ dactyl_lynx_keyboard/dactyl_lynx.py
@@ -664,6 +664,63 @@ class MiniDINConnectorMount:
         )
 
 
+class TrackPointMount:
+    def __init__(self):
+        self.trackpoint_stem_hole_radius_lower = 7.8 / 2
+        self.trackpoint_stem_hole_radius_upper = 6.5 / 2
+        self.trackpoint_screw_hole_radius = 2 / 2
+        self.trackpoint_screw_hole_offset = 18.75 / 2
+        self.trackpoint_mount_thickness = plate_thickness * 1.5
+
+        self.trackpoint_stem_radius = 0.6
+        self.trackpoint_stem_length = 25  # Slightly problematic since only barbells seem to come in this length
+        self.trackpoint_ball_radius = 1.5
+        self.trackpoint_stem_base_height = 2
+
+        self.trackpoint_board_size = (23.5, 17, 1)
+
+    def trackpoint_holes(self):
+        return (
+            cylinder_outer(self.trackpoint_stem_hole_radius_lower, 2, center=True)
+            .translate((0, 0, 0.5 - self.trackpoint_mount_thickness))
+            + cylinder_outer(self.trackpoint_stem_hole_radius_upper, self.trackpoint_mount_thickness * 2.2, center=True)
+            + cylinder_outer(self.trackpoint_screw_hole_radius, self.trackpoint_mount_thickness * 2.2, center=True)
+            .translate((self.trackpoint_screw_hole_offset, 0, 0))
+            + cylinder_outer(self.trackpoint_screw_hole_radius, self.trackpoint_mount_thickness * 2.2, center=True)
+            .translate((-self.trackpoint_screw_hole_offset, 0, 0))
+            + cube(self.trackpoint_board_size, center=True)
+            .translate((0, 0, -self.trackpoint_mount_thickness))
+        ).down(2)
+
+    def trackpoint_mount(self):
+        screw_surround_radius = self.trackpoint_screw_hole_radius * 2.5
+        cutout_x_offset = 7
+        cutout_y_offset = screw_surround_radius + 5
+        return (
+            (
+                # cylinder_outer(8, self.trackpoint_mount_thickness, center=True)
+                hull()(
+                    cylinder_outer(screw_surround_radius, self.trackpoint_mount_thickness, center=True)
+                    .translate((self.trackpoint_screw_hole_offset, 0, 0)),
+                    cylinder_outer(screw_surround_radius, self.trackpoint_mount_thickness, center=True)
+                    .translate((-self.trackpoint_screw_hole_offset, 0, 0))
+                )
+            ).translate((0, 0, self.trackpoint_mount_thickness / -2))
+            - cube((10, 10, 20), center=True).translate((cutout_x_offset, cutout_y_offset, 0))
+            - cube((10, 10, 20), center=True).translate((-cutout_x_offset, cutout_y_offset, 0))
+            - cube((10, 10, 20), center=True).translate((cutout_x_offset, -cutout_y_offset, 0))
+            - cube((10, 10, 20), center=True).translate((-cutout_x_offset, -cutout_y_offset, 0))
+        ).down(2).color((0, 1, 0))
+
+    def trackpoint_shape(self):
+        return (
+            (
+                cylinder(self.trackpoint_stem_radius, self.trackpoint_stem_length)
+                + sphere(self.trackpoint_ball_radius).translate((0, 0, self.trackpoint_stem_length / 2 + self.trackpoint_ball_radius))
+            ).translate((0, 0, self.trackpoint_stem_length / 2 - self.trackpoint_mount_thickness + self.trackpoint_stem_base_height))
+        ).down(2).color((0.8, 0.8, 0.8))
+
+
 class KeyboardAssembly:
     def __init__(self, columns=6, rows=5, use_1_5u_keys=False, use_color=False, socket_shape=mx_plate_with_backplate, wall_thickness=1.5):
         self.use_color = use_color
@@ -674,6 +731,7 @@ class KeyboardAssembly:
         self.thumb_layout = ThumbWellLayout(wall_thickness=wall_thickness)
 
         self.connector_mount = MiniDINConnectorMount()
+        self.trackpoint_mount = TrackPointMount()
 
         self.screen_size = (27.75, 39.25)
         self.screen_hole_centers = (22.5, 34.05)
@@ -728,6 +786,9 @@ class KeyboardAssembly:
                 -self.connector_mount.outerRadius() - 2
             ))
         )
+
+    def transform_trackpoint_mount(self, shape):
+        return self.finger_layout.key_place(0.5, 2.5, shape)
 
     def transform_thumb_nut1(self, shape):
         return shape \
@@ -917,6 +978,10 @@ class KeyboardAssembly:
             )
             - self.transform_connector_mount(self.connector_mount.hole())
         )
+
+        if not self.left_side:
+            shape += self.transform_trackpoint_mount(self.trackpoint_mount.trackpoint_mount())
+            shape -= self.transform_trackpoint_mount(self.trackpoint_mount.trackpoint_holes())
 
         if self.use_color:
             return shape.color((0.1, 0.1, 0.1))
@@ -1152,7 +1217,7 @@ if __name__ == "__main__":
             #+ right_thumb_part.color((0.1, 0.1, 0.1))
             #+ right_connector.color((0.4, 0.1, 0.1))
             right_single_piece.color((0.1, 0.1, 0.1))
-            #+ assembly.transform_trackpoint_mount(assembly.trackpoint_mount.trackpoint_holes()).color((1, 0, 1), alpha=0.25)
+            + assembly.transform_trackpoint_mount(assembly.trackpoint_mount.trackpoint_shape()).color((1, 0, 1))
             + right_keycaps.color((1.0, 0.98, 0.95))
             + right_pcbs.color((0, 0.4, 0))
         ).translate((100, 0, 0))
