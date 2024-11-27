c878449f8e1709ecfc1592daf47e9fefb372f826
David H. Bronke <whitelynx@gmail.com>
2024-11-27 20:23:43 +0100

    update(dactyl_lynx_keyboard.lcd_mount): added type annotations and docstrings

--- dactyl_lynx_keyboard/lcd_mount.py
+++ dactyl_lynx_keyboard/lcd_mount.py
@@ -1,8 +1,11 @@
 from solid2 import cube, hull
+from solid2.core.object_base import OpenSCADObject
 from solid2.extensions.bosl2 import screws
 
 
 class LCDMount:
+    """A mount for an LCD screen.
+    """
     def __init__(self):
         self.lcdSize = (27.8, 39.3, 1.2)          # (X, Y, Z)
         self.lcdMountingHoleCenters = (22.5, 34)  # (X, Y)
@@ -10,14 +13,18 @@ class LCDMount:
         self.frameThickness = (4, 3)              # (X/Y, Z)
         self.cornerSize = 3.75
 
-    def place(self, shape):
+    def place(self, shape: OpenSCADObject) -> OpenSCADObject:
+        """Place the given shape at the location and orientation of this LCD mount.
+        """
         return (
             shape
             .rotate(20, (1, 0, 0))
             .translate((-25, 48, 50))
         )
 
-    def frame(self):
+    def frame(self) -> OpenSCADObject:
+        """Render the frame for the LCD screen.
+        """
         cornerBlockSize = self.cornerSize + self.frameThickness[0] / 2
         cornerBlockThickness = self.frameThickness[1] - self.lcdSize[2]
 
@@ -67,7 +74,9 @@ class LCDMount:
             .translate((-self.lcdMountingHoleCenters[0] / 2, self.lcdMountingHoleCenters[1] / 2, 0))
         )
 
-    def mount(self, target):
+    def mount(self, target: OpenSCADObject) -> OpenSCADObject:
+        """Render a mounting piece between the frame and the given target.
+        """
         return hull()(
             self.place(
                 cube((
